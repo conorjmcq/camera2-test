ebf52dc904f4c325d20ec10c321d9a3a8b4715b2
Conor McQ <conor.mcquillan@mastercard.com>
2018-12-19 14:30:00 +0000

    Add

---
+++ Camera2Test/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_conorjmcq_github_com_camera2test_AndroidCamera2Api_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
